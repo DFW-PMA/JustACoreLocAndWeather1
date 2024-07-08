46b1e2e1a520a1be2d7a78ae699bc496ec66ba2b
cxdcox <daryl.cox@checkmarx.com>
2024-07-07 19:18:12 -0500

    Initial Commit

---
+++ JustACoreLocation2/JustACoreLocation2App.swift
@@ -0,0 +1,17 @@
+//
+//  JustACoreLocation2App.swift
+//  JustACoreLocation2
+//
+//  Created by Daryl Cox on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct JustACoreLocation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
