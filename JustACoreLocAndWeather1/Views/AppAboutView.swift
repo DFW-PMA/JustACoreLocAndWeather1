6951af5432087f9d94dfb335c2c8db15ca49b408
DarylRCox1956 <dcox@justmacapps.net>
2024-08-05 11:48:24 -0500

    updates

--- JustACoreLocAndWeather1/Views/AppAboutView.swift
+++ JustACoreLocAndWeather1/Views/AppAboutView.swift
@@ -16,7 +16,7 @@ struct AppAboutView: View
     {
         
         static let sClsId          = "AppAboutView"
-        static let sClsVers        = "v1.0101"
+        static let sClsVers        = "v1.0102"
         static let sClsDisp        = sClsId+".("+sClsVers+"): "
         static let sClsCopyRight   = "Copyright (C) JustMacApps 2023-2024. All Rights Reserved."
         static let bClsTrace       = true
@@ -32,6 +32,16 @@ struct AppAboutView: View
 
     @Environment(\.dismiss) var dismiss
     
+#if os(macOS)
+
+    private let pasteboard = NSPasteboard.general
+
+#elseif os(iOS)
+
+    private let pasteboard = UIPasteboard.general
+
+#endif
+
     var body: some View 
     {
         
@@ -42,6 +52,30 @@ struct AppAboutView: View
 
             Divider()
             
+            if #available(iOS 17.0, *)
+            {
+
+                Image(ImageResource(name: "AppIcon@3x", bundle: Bundle.main))
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal)
+                        { size, axis in
+                            size * 0.2
+                        }
+
+            }
+            else
+            {
+
+                Image(ImageResource(name: "AppIcon@3x", bundle: Bundle.main))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100, height: 100, alignment:.center)
+
+            }
+
+            Spacer(minLength:10)
+
             HStack
             {
                 
@@ -98,7 +132,114 @@ struct AppAboutView: View
 
         Spacer()
 
+        HStack(alignment:.center)
+        {
+
+            Spacer()
+
+            VStack(alignment:.center)
+            {
+
+                Text("'JustACoreLocAndWeather1' App information:")
+                    .bold()
+                    .controlSize(.regular)
+
+            //  Text("\(ClassInfo.sClsDisp):body(some View)")
+            //      .italic()
+            //      .controlSize(.mini)
+
+                Text("\(ClassInfo.sClsCopyRight)")
+                    .italic()
+                    .controlSize(.mini)
+
+            }
+
+            Spacer()
+
+        }
+
+    //  Spacer()
+
+        HStack(alignment:.center)
+        {
+
+            Spacer()
+
+            Text(" - - - - - - - - - - - - - - - - - - - - ")
+                .bold()
+                .controlSize(.regular)
+
+            Spacer()
+
+        }
+
+    //  Spacer()
+
+        HStack(alignment:.center)
+        {
+
+            Spacer()
+
+            VStack(alignment:.center)
+            {
+
+                Text("'JustACoreLocAndWeather1' App LOG file at:")
+                    .bold()
+                    .controlSize(.regular)
+                    .contextMenu
+                    {
+
+                        Button
+                        {
+
+                            let _ = xcgLoggerMsg(sMessage:"...\(ClassInfo.sClsDisp):AppAboutView in Text.contextMenu.'copy' button #1...")
+
+                            copyLogFilespecToClipboard()
+
+                        }
+                        label:
+                        {
+
+                            Text("Copy to Clipboard")
+
+                        }
+
+                    }
+
+            //  Text("\(JustACoreLocAndWeather1AppDelegate.ClassSingleton.appDelegate!.sAppDelegateLogFilespec!)")
+                Text("\(self.appDelegate.sAppDelegateLogFilespec!)")
+                    .italic()
+                    .controlSize(.mini)
+
+            }
+
+            Spacer()
+
+        }
+
+        Spacer()
+
     }
+    
+    func copyLogFilespecToClipboard()
+    {
+        
+        xcgLoggerMsg(sMessage:"...\(ClassInfo.sClsDisp):AppAboutView in 'copyLogFilespecToClipboard()' for text of [\(self.appDelegate.sAppDelegateLogFilespec!)]...")
+        
+    #if os(macOS)
+
+        pasteboard.prepareForNewContents()
+        pasteboard.setString(self.appDelegate.sAppDelegateLogFilespec!, forType: .string)
+
+    #elseif os(iOS)
+
+        pasteboard.string = self.appDelegate.sAppDelegateLogFilespec!
+
+    #endif
+
+        return
+
+    }   // End of func copyLogFilespecToClipboard().
     
     func xcgLoggerMsg(sMessage:String)
     {
